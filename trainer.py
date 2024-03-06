6eb29f6b709e9c0a51f261b7793257f809ef9daf
krischal111 <67739761+krischal111@users.noreply.github.com>
2024-03-07 01:02:34 +0545

    Added some important stuffs

--- trainer.py
+++ trainer.py
@@ -6,6 +6,7 @@ from torch.optim import Adam
 # Side Modules
 from time import time
 from importlib import reload
+from pathlib import Path
 
 # My Modules
 import dataset
@@ -83,5 +84,42 @@ def validate(model, lossfn, validation_loader, debug=False):
         
         return time, loss
 
+def save_model(model : nn.Module, name : str, statistics : dict = None):
+    ''' 
+    model: The model to be saved.
+    statistics: The dictionary type with series of train losses and validation losses.
+    name: Name of the model to be saved.
 
+    Effect 1: Model's state dict will be saved at the f"models/{name}/model_state_dict.pth"
+    Effect 2: Statistics would be saved to f"models/{name}/statistics.pth"
+    Return value: None
+    '''
+
+    model_directory = Path("models") / Path(name)
+    model_directory.mkdir(parents=True, exist_ok=True)
+    model_file = model_directory / Path("model_state_dict.pth")
+    statistics_file = model_directory / Path("statistics.pth")
+
+    torch.save(model.state_dict(), model_file)
+    print(f"Saved model to {model_file}")
+    if statistics is not None:
+        torch.save(statistics, statistics_file)
+        print(f"Saved statistics to {statistics_file}")
+
+def retrieve_model(model, model_path):
+    ''' 
+    model: The model whose weights are to be loaded.
+    model_path: The path where the model is located (its weights and stats)
+
+    Effect: model will have its weights initialized
+    Return value: statistics file
+    '''
+    model_path = Path(model_path)
+    model_file = model_path / Path("model_state_dict.pth")
+    statistics_file = model_path / Path("statistics.pth")
+
+    stats = torch.load(statistics_file)
+    model.load_state_dict(torch.load(model_file))
+
+    return stats
     
