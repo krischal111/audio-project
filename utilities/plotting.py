33ede66a59dfc46f1b98c3af1f877f957230b1dd
krischal111 <67739761+krischal111@users.noreply.github.com>
2024-03-10 12:06:53 +0545

    codes

--- utilities/plotting.py
+++ utilities/plotting.py
@@ -16,3 +16,24 @@ def plot_waveform(waveform, sample_rate):
         if num_channels > 1:
             axes[c].set_ylabel(f"Channel {c+1}")
     figure.suptitle("waveform")
+
+def plot_statistics(statistics):
+
+    tl = statistics['TrainLosses']
+    vl = statistics['ValidationLosses']
+    time_axis = torch.arange(0,len(tl))/len(tl)
+
+    waveform = [tl, vl]
+    names = ["Train Loss", "Validation Loss"]
+
+    figure, axes = plt.subplots(2, 1)
+    num_channels = 2
+
+    if num_channels == 1:
+        axes = [axes]
+    for c in range(num_channels):
+        axes[c].plot(time_axis, waveform[c], linewidth=1)
+        axes[c].grid(True)
+        if num_channels > 1:
+            axes[c].set_ylabel(f"{names[c]}")
+    figure.suptitle("Training and validation losses")
